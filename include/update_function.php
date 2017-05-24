1a7047917d9953073882492f41d3a9f7386cf830
mambax7 <mambax7@gmail.com>
2017-05-24 00:37:50 -0400

    Remove pass-by-reference instances

--- include/update_function.php
+++ include/update_function.php
@@ -5,7 +5,7 @@ function xoops_module_update_club(&$xoopsModule, $oldVersion = null) {
 
 	if($oldVersion < 101) {
 
-		$db =& Database::getInstance();
+		$db = Database::getInstance();
 		$sql = "ALTER TABLE `".$db->prefix('club_equipe')."` ADD `equipe_class_url` VARCHAR( 255 ) NOT NULL AFTER `equipe_rank_in_club` ;";
 		$db->query($sql);
 		$sql = "ALTER TABLE `".$db->prefix('club_equipe')."` ADD `equipe_resultat_url` VARCHAR( 255 ) NOT NULL AFTER `equipe_class_url` ;";
@@ -15,7 +15,7 @@ function xoops_module_update_club(&$xoopsModule, $oldVersion = null) {
 	
 	if($oldVersion < 103) {
 		
-		$db =& Database::getInstance();
+		$db = Database::getInstance();
 		$sql = "ALTER TABLE `".$db->prefix('club_membre')."` ADD `membre_moyd_s` FLOAT NOT NULL AFTER `membre_moy_m` ;";
 		$db->query($sql);
 		$sql = "ALTER TABLE `".$db->prefix('club_membre')."` ADD `membre_moyd_d` FLOAT NOT NULL AFTER `membre_moyd_s` ;";
@@ -31,7 +31,7 @@ function xoops_module_update_club(&$xoopsModule, $oldVersion = null) {
 	
 	if($oldVersion < 104) {
 		
-		$db =& Database::getInstance();
+		$db = Database::getInstance();
 		$sql = "CREATE TABLE `".$db->prefix('club_membretournoi')."` (`membre_licence` int(11) NOT NULL,`tournoi_id` int(11) NOT NULL,`membretournoi_is_simple` tinyint(1) NOT NULL default '0',  `membretournoi_s_serie` enum('NC','D','C','B','A') NOT NULL,`membretournoi_is_double` tinyint(1) NOT NULL default '0',`membretournoi_d_partenaire` int(11) NOT NULL default '0',`membretournoi_d_serie` enum('NC','D','C','B','A') NOT NULL,`membretournoi_is_mixte` tinyint(1) NOT NULL default '0',`membretournoi_m_partenaire` int(11) NOT NULL default '0',`membretournoi_m_serie` enum('NC','D','C','B','A') NOT NULL,PRIMARY KEY  (`membre_licence`,`tournoi_id`),UNIQUE KEY `unique_double` (`membre_licence`,`tournoi_id`,`membretournoi_is_double`),UNIQUE KEY `unique_simple` (`membre_licence`,`tournoi_id`,`membretournoi_is_simple`),UNIQUE KEY `unique_mixte` (`membre_licence`,`tournoi_id`,`membretournoi_is_mixte`)) ;";
 		$db->query($sql);
 		$sql = "CREATE TABLE `".$db->prefix('club_palmares')."` (`membre_licence` int(11) NOT NULL,`tournoi_id` int(11) NOT NULL,`palmares_s` tinyint(4) NOT NULL default '0',`palmares_d` tinyint(4) NOT NULL default '0',`palmares_m` tinyint(4) NOT NULL default '0',PRIMARY KEY  (`membre_licence`,`tournoi_id`)) ;";
